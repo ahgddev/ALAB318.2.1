4c63d72ea2b14f0dee69e76014e47ef514eb66ce
ahgddev <ahgddev@gmail.com>
2024-02-19 17:15:34 -0700

    Modifying views

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const pug = require('pug');
 const app = express();
 const PORT = 3000;
 
+let fruits = ["apple","banana","grape"];
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-    res.send('Successful!');
-  });
+    res.render('index', { title: 'Fruits Page' })
+  })
+
+
 
 app.listen(PORT, () => console.log('App is running on Port: ' + PORT));
