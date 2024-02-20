3d9223f33f84805281ff8826d3fe7edfacce05eb
ahgddev <ahgddev@gmail.com>
2024-02-19 21:04:50 -0700

    Finished lab, renamed images.

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const bodyParser = require('body-parser');
 const pug = require("pug");
 const app = express();
 const PORT = 3000;
+let factObj = {};
 const fruitFacts = [
   {
     fruitName: "apple",
     f1: "Apple trees take 4-5 years to produce their first fruit. A standard size apple tree starts bearing fruit 8-10 years after it is planted. A dwarf tree starts bearing fruit in 3-5 years.",
     f2: "Apples will ripen six to ten times faster at room temperature than if they were refrigerated.",
     f3: "It takes about 36 apples to create one gallon of apple cider.",
-    url: "/greenapple.jpg"
+    url: "/apple.jpg"
   },  {
     fruitName: "banana",
     f1: "In 2012, a resident of Illinois managed to break a world record by peeling and consuming 8 whole bananas in 60 seconds.",
@@ -21,7 +22,7 @@ const fruitFacts = [
     f1: "It takes about 2.5 pounds of grapes to make one bottle of wine.",
     f2: "There are more than 8,000 grape varieties from about 60 species. The main types are American and European.",
     f3: "Grapes come in many colors, including green, red, black, yellow, pink, and purple. `White` grapes are actually green.",
-    url: "/grapes.jpg"
+    url: "/grape.jpg"
   }
 ];
 
@@ -31,17 +32,23 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(express.static('./images'));
 
+app.get("/imagedownload", (req, res) => {
+    res.download(`./images/${factObj.fruitName}.jpg`);
+})
+
 app.get("/", (req, res) => {
   res.render("index", { title: "Fruits Page" });
 });
 
+
 app.get("/:fruit", (req, res) => {
   let fruit = req.params.fruit;
-  let factObj = fruitFacts.find(fName => fName.fruitName == fruit);
+  factObj = fruitFacts.find(fName => fName.fruitName == fruit);
   res.render("fruit", { title: `${fruit} Page`, fruit: `${fruit}`, fact1: factObj.f1, fact2: factObj.f2, fact3: factObj.f3, url: factObj.url});
 })
 .post('/emailsignup', function(req,res){
     console.log(req.body);
   })
+
 
 app.listen(PORT, () => console.log("App is running on Port: " + PORT));
